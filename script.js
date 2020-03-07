c90a84f88e74b77530013934fc6ccd690d890cf5
Lera Green <zelnayavaleriya@gmail.com>
2020-03-07 21:13:50 +0300

    add cat movement to the right

--- script.js
+++ script.js
@@ -1,8 +1,34 @@
 const canvas = document.getElementById('field');
 const ctx = canvas.getContext('2d');
 
-ctx.beginPath();
-ctx.arc(50, 50, 30, 0, Math.PI*2);
-ctx.fillStyle = '#260602';
-ctx.fill();
-ctx.closePath();
+const cat = {
+  name: "Bite",
+  position: {
+    x: 50,
+    y: 50
+  }
+}
+
+const renderCat = ({x, y}) => {
+  ctx.clearRect(0, 0, 700, 700);
+
+  ctx.beginPath();
+  ctx.arc(x, y, 30, 0, Math.PI*2);
+  ctx.fillStyle = '#260602';
+  ctx.fill();
+  ctx.closePath();
+}
+
+renderCat(cat.position);
+
+
+const cell = 10;
+
+document.addEventListener('keydown', (evt) => {
+  if (evt.key === 'ArrowRight') {
+    cat.position.x += cell;
+    renderCat(cat.position);
+    console.log('Байтян бежит вправо');
+  }
+  // console.log(evt);
+});
